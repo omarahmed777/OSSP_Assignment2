25aa2ac921a497a575e6f6e1573f6554418fb930
omarahmed777 <oa250303@gmail.com>
2022-11-03 18:03:35 +0000

    Committed new model answer

--- bst.c
+++ bst.c
@@ -2,8 +2,6 @@
 #include<stdlib.h>
 #include<string.h>
 #include <unistd.h>
-#include <assert.h>
-#include <stdbool.h>
 #include "bst.h"
 
 /*
@@ -11,138 +9,156 @@
 Place for the BST functions from Exercise 1.
 
 */
-/*struct _Node {
+struct _Node {
     int value;
-    Node * left, * right;
-};*/
+    struct _Node* left;
+    struct _Node* right;
+};
 
-/*
-   Returns the parent of an either existing or hypothetical node with the given value
- */
-Node * find_parent(Node * root, int value) {
-    assert(root != NULL);
-    assert(value != root->data);
+Node* addNode(Node *root, int value)
+{
+    Node *newNode = malloc(sizeof(Node));
+    newNode->data = value;
+    newNode->left = NULL;
+    newNode->right = NULL;
 
-    //If value < x, go right, else go left
-    Node * next = value < root->data ? root->right : root->left;
+    Node *temp;
+    Node *prev = NULL;
 
-    if (next == NULL || next->data == value)
-        return root;
+    temp = root;
+    while(temp) {
+        prev = temp;
+        if (temp->data > newNode->data)
+            temp = temp->right;
+        else if (temp->data < newNode->data)
+            temp = temp->left;
+        else {
+            free(newNode);
+            return NULL;
+        }
+    }
+    if (prev==NULL)
+        root = newNode;
+    else {
+        if (prev->data > newNode->data)
+            prev->right = newNode;
+        else
+            prev->left = newNode;
+    }
+
+    return newNode;
+}
+
+Node* freeSubtree(Node *N) {
+    if (N == NULL)
+        return NULL;
+    freeSubtree(N->left);
+    freeSubtree(N->right);
+    free(N);
+}
+
+void removeSubtreeWorker(Node *R, int value) {
+    if (R == NULL)
+        return;
+
+    if (R->left && R->left->data == value) {
+        freeSubtree(R->left);
+        R->left = NULL;
+        return;
+    }
+
+    if (R->right && R->right->data == value) {
+        freeSubtree(R->right);
+        R->right = NULL;
+        return;
+    }
+
+    if (R->data < value)
+        removeSubtreeWorker(R->left, value);
     else
-        return find_parent(next, value);
+        removeSubtreeWorker(R->right, value);
 }
 
-/*
-   Constructs a new node
- */
-Node * mk_node(int value) {
-    Node * node = (Node *) malloc(sizeof(Node));
-    node->data = value;
-    node->left = node->right = NULL;
-    return node;
-}
-
-Node * addNode(Node * root, int value) {
-    if (root == NULL)
-        return mk_node(value);
-
-    if (value == root->data)
+Node* removeSubtree(Node *R, int value) {
+    if (R == NULL)
         return NULL;
 
-    Node * parent = find_parent(root, value);
-    Node * child = value > parent->data ? parent->left : parent->right;
-    assert(child == NULL || child->data == value);
-
-    if (child == NULL) {
-        // value not found, then insert and return node
-        child = mk_node(value);
-        if (value > parent->data)
-            parent->left = child;
-        else
-            parent->right = child;
-
-        return child;
-    } else {
-        // value found, then return null
+    if (R->data == value) {
+        freeSubtree(R);
         return NULL;
     }
+    removeSubtreeWorker(R, value);
+    return R;
 }
 
-bool is_ordered(Node * root) {
-    if (root == NULL)
-        return true;
-    if (root->left && root->left->data < root->data)
-        return false;
-    if (root->right && root->right->data > root->data)
-        return false;
-    return true;
-}
-
-Node * removeNode(Node * root, int value) {
-    assert(is_ordered(root));
-
-    // empty tree
-    if (root == NULL)
-        return NULL;
-
-    // find node with value 'value' and its parent node
-    Node * parent, * node;
-    if (root->data == value) {
-        parent = NULL;
-        node = root;
-    } else {
-        parent = find_parent(root, value);
-        node = value > parent->data ? parent->left : parent->right;
+void displaySubtree(Node *N)
+{
+    if (N==NULL)
+    {
+        return ;
     }
-    assert(node == NULL || node->data == value);
-
-    // value not found
-    if (node == NULL)
-        return root;
-
-    // re-establish consistency
-    Node * new_node;
-    if (node->left == NULL) {
-        // node has only right child, then make right child the new node
-        new_node = node->right;
-    } else {
-        // otherwise make right child the rightmost leaf of the subtree rooted in the left child
-        // and make the left child the new node
-        Node * rightmost = new_node = node->left;
-        while (rightmost->right != NULL)
-            rightmost = rightmost->right;
-        rightmost->right = node->right;
-    }
-
-    free(node);
-
-    Node * new_root;
-    if (parent == NULL) {
-        // if deleted node was root, then return new node as root
-        new_root = new_node;
-    } else {
-        // otherwise glue new node with parent and return old root
-        new_root = root;
-        if (value < parent->data)
-            parent->left = new_node;
-        else
-            parent->right = new_node;
-    }
-
-    assert(is_ordered(new_root));
-
-    return new_root;
-}
-
-void displaySubtree(Node * N) {
-    if (N == NULL) return;
-
     displaySubtree(N->right);
-    printf("%d \n", N->data);
+    printf("%d\n", N->data);
     displaySubtree(N->left);
 }
 
-int numberLeaves(Node * N) {
+int countNodes(Node *N)
+{
+    int count=0;
+    if(N==NULL)
+        return 0;
+    count = count + countNodes(N->right);
+    count = count + countNodes(N->left);
+    count = count + 1;
+    return count;
+}
+
+// this is the most complicated task
+Node* removeNode(Node* root, int value)
+{
+    // Ensure root isn't null.
+    if (root == NULL) {
+        return NULL;
+    }
+
+    if (value > root->data) {  // Value is in the left sub-tree.
+        root->left = removeNode(root->left, value);
+    } else if (value < root->data) { // Value is in the right sub-tree.
+        root->right = removeNode(root->right, value);
+    } else { // Found the correct node with value
+        // Check the three cases - no child, 1 child, 2 child...
+        // No Children
+        if (root->left == NULL && root->right == NULL) {
+            free(root);
+            root = NULL;
+        }
+            // 1 child (on the right)
+        else if (root->left == NULL) {
+            Node *temp = root; // save current node
+            root = root->right;
+            free(temp);
+        }
+            // 1 child (on the left)
+        else if (root->right == NULL) {
+            Node *temp = root; // save current node
+            root = root->left;
+            free(temp);
+        }
+            // Two children
+        else {
+            // find minimal value of right subtree
+            Node *temp = root->left;
+            while(temp->right != NULL) {
+                temp = temp->right;
+            }
+            root->data = temp->data; // duplicate the node
+            root->left = removeNode(root->left, root->data); // delete the duplicate node
+        }
+    }
+    return root; // parent node can update reference
+}
+
+int numberLeaves(Node *N) {
     if (N == NULL)
         return 0;
 
@@ -152,69 +168,21 @@ int numberLeaves(Node * N) {
     return numberLeaves(N->left) + numberLeaves(N->right);
 }
 
-/*
-   Frees the entire subtree rooted in 'root' (this includes the node 'root')
- */
-Node* freeSubtree(Node * root) {
-    if (root == NULL)
-        return NULL;
-
-    freeSubtree(root->left);
-    freeSubtree(root->right);
-    free(root);
-}
-
-/*
-   Deletes all nodes that belong to the subtree (of the tree of rooted in 'root')
-   whose root node has value 'value'
- */
-Node * removeSubtree(Node * root, int value) {
-    assert(is_ordered(root));
-
-    // empty tree
-    if (root == NULL)
-        return NULL;
-
-    // entire tree
-    if (root->data == value) {
-        freeSubtree(root);
-        return NULL;
-    }
-
-    // free tree rooted in the left or right node and set the respective pointer to NULL
-    Node * parent = find_parent(root, value);
-    if (value > parent->data) {
-        assert(parent->left == NULL || parent->left->data == value);
-        freeSubtree(parent->left);
-        parent->left = NULL;
-    } else {
-        assert(parent->right == NULL || parent->right->data == value);
-            freeSubtree(parent->right);
-        parent->right = NULL;
-    }
-
-    return root;
-}
-
-/*
-   Compute the depth between root R and node N
-
-   Returns the number of edges between R and N if N belongs to the tree rooted in R,
-   otherwise it returns -1
- */
-int nodeDepth (Node * R, Node * N) {
+int nodeDepth(Node *R, Node *N) {
     if (R == NULL || N == NULL)
         return -1;
+
     if (R == N)
         return 0;
 
-    int sub_depth = nodeDepth(R->data < N->data ? R->left : R->right, N);
+    int subDepth = nodeDepth(R->data < N->data ? R->left : R->right, N);
 
-    if (sub_depth >= 0)
-        return sub_depth + 1;
+    if (subDepth >= 0)
+        return subDepth + 1;
     else
         return -1;
 }
+
 
 /*
  * ------------------------------ EXERCISE 2 ------------------------------
@@ -234,12 +202,12 @@ int sum(Node *N) {
  * Returns total number of nodes in tree
  * Recursion atm, could take a while potentially with large trees?
  */
-int countNodes(Node *root) {
+/*int countNodes(Node *root) {
     if (root == NULL)
         return 0;
     else
         return 1 + countNodes(root->left) + countNodes(root->right); //Order of traversal doesn't matter
-}
+}*/
 
 float avgSubtree(Node *N)
 {
@@ -275,8 +243,8 @@ Node* balanceTreeUtil(Node* root, int arr[], int start, int end) {
         return NULL;
     int mid = (start+end) / 2; //Get middle elem of arr
     root->data = arr[mid]; //Middle elem of sorted array is root
-    root->right = balanceTreeUtil(root, arr, start, mid-1); //Left half of array will be right children
-    root->left = balanceTreeUtil(root, arr, mid-1, end); //Right half of array will be left children
+    root->right = balanceTreeUtil(root->right, arr, start, mid-1); //Left half of array will be right children
+    root->left = balanceTreeUtil(root->left, arr, mid-1, end); //Right half of array will be left children
     return root;
 }
 // This functions converts an unbalanced BST to a balanced BST
